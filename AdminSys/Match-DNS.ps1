84cb22acc0332cb6e1cce1ba3d822dfa59c2cf37
0xF4D3 <104458102+0xF4D3@users.noreply.github.com>
2022-04-26 23:11:46 +0200

    Help
    Voila

--- AdminSys/Match-DNS.ps1
+++ AdminSys/Match-DNS.ps1
@@ -1,6 +1,25 @@
+<#
+ .Synopsis
+  Ce module sert à croiser les informations DNS avec les adresses IP dans 2 fichiers differents. 
+  Cela est utile lorsque nous faire un check de nos DNS avec des IP pour voir si il y'a correspondance ou pas.
+
+ .Description
+  C'est outil utilise 2 entrées 1. L'export DNS 2. Une liste d'adresse IP. L'objectif est de matcher ces IP avec les noms de domaines
+
+ .Parameter IPExtract
+  Le chemin de notre fichier contenant les adresses IP
+
+ .Parameter DNSExtract
+  Le chemin de notre fichier contenant les adresses DNS
+
+ .Example
+   match-DNS -DnsExtract .\DNSRecord.txt -IpExtract .\IpFile.csv
+#>
 Function Match-DNS{
 param(
+    [parameter (Mandatory=$true)]
     [string] $DnsExtract,
+    [parameter (Mandatory=$true)]
     [string] $IpExtract
 )
 
@@ -56,8 +75,8 @@ if(($DnsTypeOfFileIsCSV -eq 1)-and ($IpTypeOfFileIsCSV -eq 1)){
     foreach($lineOfIpFile in $importIPextract){
         write-host "test of ip" $lineOfIpFile;
         foreach($lineOfDNSFile in $my_FileDns){
-            $iponthisline = (echo $lineOfDNSFile | Select-String -Pattern "\d{1,3}(\.\d{1,3}){3}" -AllMatches).Matches.Value;
-            $dnsonthisline = (echo $lineOfDNSFile | Select-String -Pattern "^\D+?\S*\.\D+\." -AllMatches).Matches.Value;
+            $iponthisline = (Write-Output $lineOfDNSFile | Select-String -Pattern "\d{1,3}(\.\d{1,3}){3}" -AllMatches).Matches.Value;
+            $dnsonthisline = (Write-Output $lineOfDNSFile | Select-String -Pattern "^\D+?\S*\.\D+\." -AllMatches).Matches.Value;
             if($lineOfIpFile.IP -eq $iponthisline){
                 $Matching.Add($iponthisline,$dnsonthisline)
                 write-host "My Ip File" $lineOfIpFile.IP "match with" $iponthisline "and with this dns record" $dnsonthisline
