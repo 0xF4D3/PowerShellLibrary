58cd16d33038e3b37626449cb8d92cb33ffe72a6
0xF4D3 <104458102+0xF4D3@users.noreply.github.com>
2022-04-26 22:57:19 +0200

    Regex Explode like my brain
    Regex string pour le parsing des fichiers txt. Fonctionnalité pour tous les noms de domaines/Subdomains

--- AdminSys/Match-DNS.ps1
+++ AdminSys/Match-DNS.ps1
@@ -11,6 +11,9 @@ param(
 $DnsTypeOfFileIsCSV = 0
 $IpTypeOfFileIsCSV = 0
 
+##Futur valeur de retour, une hashtable qui contiendra, DNS et IP
+$Matching = @{};
+
 
 ### Get extension
 $DnsTypeOfFile = $DnsExtract.LastIndexOf('.');
@@ -51,20 +54,21 @@ if(($DnsTypeOfFileIsCSV -eq 1)-and ($IpTypeOfFileIsCSV -eq 1)){
     ###Extact Ip address from line
     $my_FileDns = Get-Content $DnsExtract
     foreach($lineOfIpFile in $importIPextract){
-    write-host "test"
-    sleep(2)
+        write-host "test of ip" $lineOfIpFile;
         foreach($lineOfDNSFile in $my_FileDns){
-            $iponthisline = (echo $lineOfDNSFile | Select-String -Pattern "\d{1,3}(\.\d{1,3}){3}" -AllMatches).Matches.Value
-            $dnsonthisline = (echo $lineOfDNSFile | Select-String -Pattern "\D{1,}\." -AllMatches).Matches.Value
+            $iponthisline = (echo $lineOfDNSFile | Select-String -Pattern "\d{1,3}(\.\d{1,3}){3}" -AllMatches).Matches.Value;
+            $dnsonthisline = (echo $lineOfDNSFile | Select-String -Pattern "^\D+?\S*\.\D+\." -AllMatches).Matches.Value;
             if($lineOfIpFile.IP -eq $iponthisline){
+                $Matching.Add($iponthisline,$dnsonthisline)
                 write-host "My Ip File" $lineOfIpFile.IP "match with" $iponthisline "and with this dns record" $dnsonthisline
             }
 
         }
     }
-    Write-Host $ipValueDns -ForegroundColor DarkMagenta
+    $Matching
 
 }else{
+    ## Les Deux fichiers sont des TXT
 
 }
 }
